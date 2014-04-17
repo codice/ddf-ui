f748b9a5f9f03ea83585d73d6248ab068bbf1dab
Scott Tustison <scott.a.tustison@lmco.com>
2014-04-17 09:04:10 -0700

    DDF-399 moved application code into a separate module

--- ui/src/main/webapp/config.js
+++ ui/src/main/webapp/config.js
@@ -41,7 +41,15 @@
             css: 'lib/require-css/css',
 
             // default admin ui
-            app: 'js/application'
+            app: 'js/application',
+
+            // templates
+            applicationTemplate: '/applications/templates/application.handlebars',
+            applicationNodeTemplate: '/applications/templates/applicationNode.handlebars',
+            detailsTemplate: '/applications/templates/details.handlebars',
+            applicationNew: '/applications/templates/applicationNew.handlebars',
+            mvnItemTemplate: '/applications/templates/mvnUrlItem.handlebars',
+            fileProgress: '/applications/templates/fileProgress.handlebars'
         },
 
 
