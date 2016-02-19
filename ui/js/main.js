3657e64e917695f145ca85aa5785a7bffc333449
Andrew Fiedler <andrewkfiedler@gmail.com>
2016-02-19 15:15:14 -0700

    Added some scaffolding

---
+++ ui/js/main.js
@@ -0,0 +1,159 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details. A copy of the GNU Lesser General Public License is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ *
+ **/
+require.config({
+  paths: {
+    bootstrap: 'lib/components-bootstrap/js/bootstrap.min',
+    bootstrapselect: 'lib/bootstrap-select/dist/js/bootstrap-select.min',
+
+    cometd: 'lib/cometd/org/cometd',
+    jquerycometd: 'lib/cometd/jquery/jquery.cometd',
+    moment: 'lib/moment/min/moment.min',
+    perfectscrollbar: 'lib/perfect-scrollbar/min/perfect-scrollbar.min',
+    spin: 'lib/spin.js/spin',
+    q: 'lib/q/q',
+
+    // backbone
+    backbone: 'lib/components-backbone/backbone-min',
+    backbonecometd: 'lib/backbone-cometd/backbone.cometd.extension',
+    underscore: 'lib/lodash/lodash.min',
+    marionette: 'lib/marionette/lib/backbone.marionette.min',
+    // TODO test combining
+    modelbinder: 'lib/backbone.modelbinder/Backbone.ModelBinder.min',
+    collectionbinder: 'lib/backbone.modelbinder/Backbone.CollectionBinder.min',
+
+    // application
+    application: 'js/application',
+    cometdinit: 'js/cometd',
+    direction: 'js/direction',
+    webglcheck: 'js/webglcheck',
+    twodcheck: 'js/2dmapcheck',
+    maptype: 'js/maptype',
+    spinnerConfig: 'js/spinnerConfig',
+    wreqr: 'js/wreqr',
+    properties: 'properties',
+
+    // jquery
+    jquery: 'lib/jquery/jquery.min',
+    'jquery.ui.widget': 'lib/jquery-ui/ui/minified/jquery.ui.widget.min',
+
+    // handlebars
+    handlebars: 'lib/handlebars/handlebars.min',
+    icanhaz: 'lib/icanhandlebarz/ICanHandlebarz',
+
+    // require plugins
+    text: 'lib/requirejs-plugins/lib/text',
+    css: 'lib/require-css/css.min',
+
+    // map
+    cesium: 'lib/cesiumjs/Cesium/Cesium',
+    drawHelper: 'lib/cesium-drawhelper/DrawHelper',
+    openlayers: 'lib/openlayers3/build/ol',
+    usngs: 'lib/usng.js/usng',
+
+    wellknown: 'lib/wellknown/wellknown'
+  },
+
+  shim: {
+    backbone: {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
+    },
+    backbonecometd: ['underscore', 'jquery', 'backbone', 'cometdinit'],
+    marionette: {
+      deps: ['jquery', 'underscore', 'backbone'],
+      exports: 'Marionette'
+    },
+
+    underscore: {
+      exports: '_'
+    },
+
+    handlebars: {
+      exports: 'Handlebars'
+    },
+
+    cesium: {
+      exports: 'Cesium'
+    },
+    drawHelper: {
+      deps: ['cesium'],
+      exports: 'DrawHelper'
+    },
+    openlayers: {
+      exports: 'ol'
+    }
+
+  },
+
+  waitSeconds: 0
+})
+
+require.onError = function (err) {
+  if (typeof console !== 'undefined') {
+    console.error('RequireJS failed to load a module', err)
+  }
+}
+
+require(['underscore',
+  'jquery',
+  'backbone',
+  'marionette',
+  'application',
+  'icanhaz',
+  'properties',
+  'js/HandlebarsHelpers',
+  'js/ApplicationHelpers'],
+  function (_, $, Backbone, Marionette, app, ich, properties) {
+    'use strict'
+
+    // Make lodash compatible with Backbone
+    var lodash = _.noConflict()
+    _.mixin({
+      'debounce': _.debounce || lodash.debounce,
+      'defer': _.defer || lodash.defer,
+      'pluck': _.pluck || lodash.pluck
+    })
+
+    var document = window.document
+
+    // in here we drop in any top level patches, etc.
+
+    Marionette.Renderer.render = function (template, data) {
+      if (!template) {
+        return ''
+      }
+      return ich[template](data)
+    }
+
+    // TODO: this hack here is to fix the issue of the main div not resizing correctly
+    // when the header and footer are in place
+    // remove this code when the correct way to get the div to resize is discovered
+    $(window).resize(function () {
+      var height = $('body').height()
+      if (properties.ui.header && properties.ui.header !== '') {
+        height = height - 20
+      }
+      if (properties.ui.footer && properties.ui.footer !== '') {
+        height = height - 20
+      }
+      $('#content').height(height)
+    })
+
+    $(window).trigger('resize')
+
+    $(document).ready(function () {
+      document.title = properties.branding
+    })
+
+    // Actually start up the application.
+    app.App.start({})
+  })
