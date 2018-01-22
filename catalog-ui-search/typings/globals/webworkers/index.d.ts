16b9421244920f452a7838152d4cb3304a7d2c78
Chris Badahdah <a.s.badahdah@gmail.com>
2018-01-22 11:58:22 -0700

    DDF-3584 Moves ui sources to ui module
    - Slightly restructure catalog-ui-search for easier packaging

--- catalog-ui-search/typings/globals/webworkers/index.d.ts
+++
@@ -1,2 +0,0 @@
-declare var global:any
-declare function postMessage(message: any, transfer?: any[]): void;  //https://www.antonmata.me/2017/04/04/web-workers-vscode-intellisense.html
--- catalog-ui-search/typings/index.d.ts
+++
@@ -1,1 +0,0 @@
-/// <reference path="globals/webworkers/index.d.ts" />
