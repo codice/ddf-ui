83d70df323ef2ae5f80f1472abca1a4109d387e7
Andrew Zimmerman <andrew.zimmerman@connexta.com>
2020-01-29 14:10:36 -0700

    DDF-5495 Provide an extension for Table Export behavior

--- ui-backend/catalog-ui-search/src/main/java/org/codice/ddf/catalog/ui/util/EndpointUtil.java
+++ ui-backend/catalog-ui-search/src/main/java/org/codice/ddf/catalog/ui/util/EndpointUtil.java
@@ -80,6 +80,7 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.input.BoundedInputStream;
 import org.apache.commons.lang3.StringUtils;
+import org.codice.ddf.catalog.ui.api.EndpointUtility;
 import org.codice.ddf.catalog.ui.config.ConfigurationApplication;
 import org.codice.ddf.catalog.ui.metacard.EntityTooLargeException;
 import org.codice.ddf.catalog.ui.query.cql.CqlQueryResponse;
@@ -99,7 +100,7 @@ import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
 
-public class EndpointUtil {
+public class EndpointUtil implements EndpointUtility {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(EndpointUtil.class);
   public static final String MESSAGE = "message";
@@ -522,6 +523,7 @@ public class EndpointUtil {
     return GSON.toJson(result);
   }
 
+  @Override
   public CqlQueryResponse executeCqlQuery(CqlRequest cqlRequest)
       throws UnsupportedQueryException, SourceUnavailableException, FederationException {
     QueryRequest request = cqlRequest.createQueryRequest(catalogFramework.getId(), filterBuilder);
