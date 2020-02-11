89c279c67e2b8d6370ed877ada37fbd2058a1e40
bdeining <benjamin.deininger@connexta.com>
2020-02-11 14:06:49 -0700

    Undid erroneous change

--- ui-frontend/packages/catalog-ui-search/src/main/webapp/extension-points/user-settings/user-settings.tsx
+++ ui-frontend/packages/catalog-ui-search/src/main/webapp/extension-points/user-settings/user-settings.tsx
@@ -12,7 +12,7 @@
  * <http://www.gnu.org/licenses/lgpl.html>.
  *
  **/
-import plugin from '../../plugins/user-settings'
+import plugin from 'plugins/user-settings'
 
 import {
   default as UserSettings,
