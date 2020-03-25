445c6b34e1b0f97b448def9a16aa355cf4d81b33
Cassandra Bailey <cassandra.bailey@connexta.com>
2020-03-25 09:21:36 -0600

    DDF-UI-135 add missing parameter

--- ui-frontend/packages/catalog-ui-search/src/main/webapp/react-component/utils/export/export.tsx
+++ ui-frontend/packages/catalog-ui-search/src/main/webapp/react-component/utils/export/export.tsx
@@ -74,6 +74,12 @@ export const getExportResult = (result: any) => {
   return {
     id: getResultId(result),
     source: getResultSourceId(result),
+    attributes: Object.keys(
+      result
+        .get('metacard')
+        .get('properties')
+        .get('attributes')
+    ),
   }
 }
 
