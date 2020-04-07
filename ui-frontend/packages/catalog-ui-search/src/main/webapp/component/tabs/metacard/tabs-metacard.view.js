269ee06cd9e4a59807453974e27d686a13942e18
Jason Smith <jason.smith@connexta.com>
2020-04-07 13:54:30 -0600

    DDF-5260 Updates editing restrictions to take into account write access  - Updates the edit restriction checking to check both the admin config and write access for the result.  - Updates the usage of these restrictions to be local and removes the global 'is-editing-restricted' class that was placed on the body.  This lets the choice be decided on a per result basis and makes it so components who want to know are in charge of figuring it out.  - Adds exception for workspace type to canWrite  - Workspaces are central to the application at the moment, so there is an exception to the "noEditingAllowed" restriction for them.  They are still subject to normal security restrictions however (if shared, check if they're allowed to write).

--- ui-frontend/packages/catalog-ui-search/src/main/webapp/component/tabs/metacard/tabs-metacard.view.js
+++ ui-frontend/packages/catalog-ui-search/src/main/webapp/component/tabs/metacard/tabs-metacard.view.js
@@ -18,6 +18,7 @@ const TabsView = require('../tabs.view')
 const MetacardTabsModel = require('./tabs-metacard')
 const store = require('../../../js/store.js')
 const properties = require('../../../js/properties.js')
+const user = require('../../singletons/user-instance')
 
 module.exports = TabsView.extend({
   className: 'is-metacard',
@@ -93,7 +94,7 @@ module.exports = TabsView.extend({
       this.model.set('activeTab', 'Summary')
     }
     if (
-      properties.isEditingRestricted() &&
+      !user.canWrite(result) &&
       ['Archive', 'Overwrite'].indexOf(activeTabName) >= 0
     ) {
       this.model.set('activeTab', 'Summary')
@@ -127,10 +128,6 @@ module.exports = TabsView.extend({
     }
   },
   determineDisabledContent() {
-    this.$el.toggleClass(
-      'is-editing-disabled',
-      properties.isEditingRestricted()
-    )
     this.$el.toggleClass(
       'is-preview-disabled',
       !properties.isMetacardPreviewEnabled()
