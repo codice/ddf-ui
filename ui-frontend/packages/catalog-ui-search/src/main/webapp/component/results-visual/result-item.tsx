ea69366ad08508816987659406e9f8004d3d0cd8
andrewkfiedler <andrewkfiedler@gmail.com>
2020-08-27 23:10:21 -0700

    Fix jittery check
     - adds invisible box so check can't jitter

--- ui-frontend/packages/catalog-ui-search/src/main/webapp/component/results-visual/result-item.tsx
+++ ui-frontend/packages/catalog-ui-search/src/main/webapp/component/results-visual/result-item.tsx
@@ -506,13 +506,22 @@ export const ResultItem = ({
                 {(() => {
                   if (isSelected) {
                     return (
-                      <Box
-                        color="secondary.main"
-                        className={`transform transition duration-200 ease-in-out -translate-x-full group-1-focus-visible:translate-x-0 group-1-hover:translate-x-0`}
-                      >
-                        <CheckBoxIcon className="group-1-hover:block group-1-focus-visible:block hidden" />
-                        <CheckIcon className="group-1-hover:hidden group-1-focus-visible:hidden block" />
-                      </Box>
+                      <>
+                        <Box
+                          color="secondary.main"
+                          className={`absolute w-full h-full left-0 top-0 opacity-0 transform transition duration-200 ease-in-out -translate-x-full`}
+                        >
+                          <CheckBoxIcon className="group-1-hover:block group-1-focus-visible:block hidden" />
+                          <CheckIcon className="group-1-hover:hidden group-1-focus-visible:hidden block" />
+                        </Box>
+                        <Box
+                          color="secondary.main"
+                          className={`transform transition duration-200 ease-in-out -translate-x-full group-1-focus-visible:translate-x-0 group-1-hover:translate-x-0`}
+                        >
+                          <CheckBoxIcon className="group-1-hover:block group-1-focus-visible:block hidden" />
+                          <CheckIcon className="group-1-hover:hidden group-1-focus-visible:hidden block" />
+                        </Box>
+                      </>
                     )
                   } else if (!isSelected) {
                     return (
