ae4511db674d60229cf04192c5f01d64db131cc2
Ryunki Song <cantstoptheunk@users.noreply.github.com>
2020-09-23 12:49:44 -0600

    DDF-UI 365 Adds audit logging to DDF-UI (#373)

--- ui-frontend/packages/catalog-ui-search/src/main/webapp/component/visualization/inspector/inspector-lazy.tsx
+++ ui-frontend/packages/catalog-ui-search/src/main/webapp/component/visualization/inspector/inspector-lazy.tsx
@@ -1,9 +1,13 @@
-import * as React from 'react'
+import React from 'react'
+import { useEffect } from 'react'
 import { hot } from 'react-hot-loader'
 import { useLazyResultsFromSelectionInterface } from '../../selection-interface/hooks'
 import MRC from '../../../react-component/marionette-region-container'
 import { useSelectedResults } from '../../../js/model/LazyQueryResult/hooks'
+import { postAuditLog } from '../../../react-component/utils/audit/audit-endpoint'
 const InspectorView = require('./inspector.view')
+
+let selectedIds = new Set<string>()
 
 type Props = {
   selectionInterface: any
@@ -22,14 +26,49 @@ const LazyInspector = ({ selectionInterface }: Props) => {
   React.useEffect(() => {
     selectionInterface.setSelectedResults(backboneModels)
   })
+
+  useEffect(
+    () => {
+      let newSelectedIds = new Set(Object.keys(selectedResults))
+
+      let unselectedIds = new Set<string>()
+      if (selectedIds.size > 0) {
+        selectedIds.forEach((id: string) => {
+          if (!newSelectedIds.has(id)) {
+            unselectedIds.add(id)
+          }
+        })
+        if (unselectedIds.size > 0) {
+          postAuditLog({
+            action: 'unselected',
+            component: 'resource',
+            ids: unselectedIds,
+          })
+        }
+      }
+
+      if (newSelectedIds.size > 0) {
+        postAuditLog({
+          action: 'selected',
+          component: 'resource',
+          ids: newSelectedIds,
+        })
+        selectedIds = newSelectedIds
+      }
+    },
+    [selectedResults]
+  )
+
   return (
-    <MRC
-      key="inspector"
-      view={InspectorView}
-      viewOptions={{
-        selectionInterface,
-      }}
-    />
+    <div>
+      <MRC
+        key="inspector"
+        view={InspectorView}
+        viewOptions={{
+          selectionInterface,
+        }}
+      />
+    </div>
   )
 }
 
