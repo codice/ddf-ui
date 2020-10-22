7ca9f716f251bdc15316986d4850c957dc73ad0e
Jordan Wilking <jordan.wilking@connexta.com>
2020-10-22 13:14:18 -0700

    Pass inputRef to Blueprint input component

--- ui-frontend/packages/catalog-ui-search/src/main/webapp/component/fields/date-time-picker.tsx
+++ ui-frontend/packages/catalog-ui-search/src/main/webapp/component/fields/date-time-picker.tsx
@@ -56,6 +56,9 @@ const DateTimePicker = ({
           BPDateProps={{
             ...BPDateProps,
             className: classes,
+            inputProps: {
+              inputRef: props.inputRef,
+            },
           }}
         />
       )
