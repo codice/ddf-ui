37ee2668734d86972a634633d549cbb01316dc32
jordanwilking <jordan.wilking@connexta.com>
2021-02-08 17:01:10 -0700

    Add source id to audit log (#516)

--- ui-frontend/packages/catalog-ui-search/src/main/webapp/react-component/utils/audit/audit-endpoint.tsx
+++ ui-frontend/packages/catalog-ui-search/src/main/webapp/react-component/utils/audit/audit-endpoint.tsx
@@ -1,33 +1,24 @@
 import fetch from '../fetch'
 
+export type AuditItem = {
+  id: String
+  'source-id'?: String
+}
+
 export type AuditLog = {
   action: string
   component: string
-  ids: Set<string> | string
+  items: AuditItem[]
 }
 
-export const postAuditLog = async ({ action, component, ids }: AuditLog) => {
-  if (typeof ids === 'string') {
-    const idSet = new Set<string>()
-    idSet.add(ids)
-    const body = {
-      action: action,
-      component: component,
-      ids: idSet,
-    }
-    await fetch(`./internal/audit/`, {
-      method: 'POST',
-      body: JSON.stringify(body),
-    })
-  } else {
-    const body = {
-      action: action,
-      component: component,
-      ids: ids,
-    }
-    await fetch(`./internal/audit/`, {
-      method: 'POST',
-      body: JSON.stringify(body),
-    })
+export const postAuditLog = async ({ action, component, items }: AuditLog) => {
+  const body = {
+    action,
+    component,
+    items,
   }
+  await fetch(`./internal/audit/`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+  })
 }
