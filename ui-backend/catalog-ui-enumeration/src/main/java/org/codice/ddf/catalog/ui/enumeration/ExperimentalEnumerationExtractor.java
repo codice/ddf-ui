473088ae66bd7c01d5532ef2cef629cbae841562
Glen <17837152+glenhein@users.noreply.github.com>
2021-03-30 09:47:40 -0700

    Add support for deprecated enumerations for queries (#557)

--- ui-backend/catalog-ui-enumeration/src/main/java/org/codice/ddf/catalog/ui/enumeration/ExperimentalEnumerationExtractor.java
+++ ui-backend/catalog-ui-enumeration/src/main/java/org/codice/ddf/catalog/ui/enumeration/ExperimentalEnumerationExtractor.java
@@ -16,6 +16,7 @@ package org.codice.ddf.catalog.ui.enumeration;
 import static org.apache.commons.lang.StringUtils.isBlank;
 
 import com.google.common.collect.Sets;
+import ddf.catalog.data.AttributeDescriptor;
 import ddf.catalog.data.AttributeInjector;
 import ddf.catalog.data.Metacard;
 import ddf.catalog.data.MetacardType;
@@ -31,6 +32,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import javax.annotation.Nullable;
+import org.codice.ddf.catalog.ui.enumeration.api.DeprecatableEnumeration;
 
 /** This class is Experimental and subject to change */
 public class ExperimentalEnumerationExtractor {
@@ -40,17 +42,21 @@ public class ExperimentalEnumerationExtractor {
 
   private final List<AttributeInjector> attributeInjectors;
 
+  private final List<DeprecatableEnumeration> deprecatableEnumerations;
+
   /**
-   * @param attributeValidatorRegistry
-   * @param metacardTypes
    * @deprecated This constructor does not take into account injected attributes. The other
    *     constructor {@link #ExperimentalEnumerationExtractor(AttributeValidatorRegistry, List,
-   *     List)} should be used.
+   *     List, List)} should be used.
    */
   @Deprecated
   public ExperimentalEnumerationExtractor(
       AttributeValidatorRegistry attributeValidatorRegistry, List<MetacardType> metacardTypes) {
-    this(attributeValidatorRegistry, metacardTypes, Collections.emptyList());
+    this(
+        attributeValidatorRegistry,
+        metacardTypes,
+        Collections.emptyList(),
+        Collections.emptyList());
   }
 
   /**
@@ -61,10 +67,12 @@ public class ExperimentalEnumerationExtractor {
   public ExperimentalEnumerationExtractor(
       AttributeValidatorRegistry attributeValidatorRegistry,
       List<MetacardType> metacardTypes,
-      List<AttributeInjector> attributeInjectors) {
+      List<AttributeInjector> attributeInjectors,
+      List<DeprecatableEnumeration> deprecatableEnumerations) {
     this.attributeValidatorRegistry = attributeValidatorRegistry;
     this.metacardTypes = metacardTypes;
     this.attributeInjectors = attributeInjectors;
+    this.deprecatableEnumerations = deprecatableEnumerations;
   }
 
   public Map<String, Set<String>> getAttributeEnumerations(String attribute) {
@@ -83,11 +91,30 @@ public class ExperimentalEnumerationExtractor {
                     .flatMap(Set::stream)
                     .distinct()
                     .collect(Collectors.toMap(o -> o, o -> avr.getSuggestedValues())))
-        .reduce(
-            (m1, m2) -> {
-              m2.entrySet().forEach(e -> m1.merge(e.getKey(), e.getValue(), Sets::union));
-              return m1;
-            })
+        .reduce(this::union)
+        .orElseGet(HashMap::new);
+  }
+
+  public Map<String, Set<String>> getDeprecatedEnumerations(@Nullable String metacardType) {
+    if (isBlank(metacardType)) {
+      metacardType = MetacardImpl.BASIC_METACARD.getName();
+    }
+    MetacardType type = getTypeFromName(metacardType);
+
+    if (type == null) {
+      return new HashMap<>();
+    }
+
+    type = applyInjectors(type, attributeInjectors);
+
+    return type.getAttributeDescriptors()
+        .stream()
+        .map(this::toDeprecatedEnumeration)
+        .filter(Optional::isPresent)
+        .map(Optional::get)
+        .filter(this::isNotEmpty)
+        .map(this::toMap)
+        .reduce(this::union)
         .orElseGet(HashMap::new);
   }
 
@@ -122,12 +149,36 @@ public class ExperimentalEnumerationExtractor {
                     .flatMap(Set::stream)
                     .distinct()
                     .collect(Collectors.toMap(o -> o, o -> avr.getSuggestedValues())))
-        .reduce(
-            (m1, m2) -> {
-              m2.entrySet().forEach(e -> m1.merge(e.getKey(), e.getValue(), Sets::union));
-              return m1;
-            })
+        .reduce(this::union)
         .orElseGet(HashMap::new);
+  }
+
+  private Optional<DeprecatableEnumeration> toDeprecatedEnumeration(
+      AttributeDescriptor attributeDescriptor) {
+    return getDeprecatedAttributeEnumerations(attributeDescriptor.getName());
+  }
+
+  private boolean isNotEmpty(DeprecatableEnumeration deprecatableEnumeration) {
+    return !deprecatableEnumeration.getDeprecatedValues().isEmpty();
+  }
+
+  private Map<String, Set<String>> union(
+      Map<String, Set<String>> firstMap, Map<String, Set<String>> secondMap) {
+    secondMap.forEach((key, value) -> firstMap.merge(key, value, Sets::union));
+    return firstMap;
+  }
+
+  private Map<String, Set<String>> toMap(DeprecatableEnumeration deprecatableEnumeration) {
+    Map<String, Set<String>> map = new HashMap<>();
+    map.put(deprecatableEnumeration.getAttribute(), deprecatableEnumeration.getDeprecatedValues());
+    return map;
+  }
+
+  private Optional<DeprecatableEnumeration> getDeprecatedAttributeEnumerations(String attribute) {
+    return deprecatableEnumerations
+        .stream()
+        .filter(deprecatableEnumeration -> deprecatableEnumeration.getAttribute().equals(attribute))
+        .findFirst();
   }
 
   @Nullable
