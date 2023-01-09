9e514d2c5da20da5ca747489b0a819d915dc1e8b
andrewkfiedler <andrewkfiedler@gmail.com>
2023-01-09 11:11:44 -0700

    Remove now unnecessary expect error

--- ui-frontend/packages/catalog-ui-search/src/main/webapp/js/model/LazyQueryResult/LazyQueryResults.tsx
+++ ui-frontend/packages/catalog-ui-search/src/main/webapp/js/model/LazyQueryResult/LazyQueryResults.tsx
@@ -85,15 +85,12 @@ export class LazyQueryResults {
   }) {
     const id = Math.random().toString()
 
-    // @ts-expect-error ts-migrate(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
     this[`subscriptionsToMe.${subscribableThing}`][id] = callback
     return () => {
-      // @ts-expect-error ts-migrate(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
       delete this[`subscriptionsToMe.${subscribableThing}`][id]
     }
   }
   _notifySubscribers(subscribableThing: SubscribableType) {
-    // @ts-expect-error ts-migrate(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
     const subscribers = this[
       `subscriptionsToMe.${subscribableThing}`
     ] as SubscriptionType
