ab15f13584b778e65a1f25791ab96344bd3e40dc
Joseph North <joernorth@gmail.com>
2023-02-15 15:43:30 -0700

    Allow empty attributes to be hidden in the inspector

--- ui-frontend/packages/catalog-ui-search/src/main/webapp/js/model/User.ts
+++ ui-frontend/packages/catalog-ui-search/src/main/webapp/js/model/User.ts
@@ -120,6 +120,7 @@ const Theme = Backbone.Model.extend({
       resultPreview: ['modified'],
       resultFilter: undefined,
       resultSort: undefined,
+      'inspector-showEmpty': true,
       'inspector-summaryShown': [],
       'inspector-summaryOrder': [],
       'inspector-detailsOrder': ['title', 'created', 'modified', 'thumbnail'],
