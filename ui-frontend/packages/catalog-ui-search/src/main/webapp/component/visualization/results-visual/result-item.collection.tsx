2f63ee074d81b95a2dbc58384285717802103ac0
andrewkfiedler <andrewkfiedler@gmail.com>
2023-05-16 14:25:19 -0700

    Update react-window-components, add logic to scroll to selected result
     - The results visuals will now respond to and scroll to the selected results.
     - Wrapped the components for the autosize virtual list in memo, since we now rerender whenever selection changes.

--- ui-frontend/packages/catalog-ui-search/src/main/webapp/component/visualization/results-visual/result-item.collection.tsx
+++ ui-frontend/packages/catalog-ui-search/src/main/webapp/component/visualization/results-visual/result-item.collection.tsx
@@ -22,7 +22,10 @@ import { LazyQueryResult } from '../../../js/model/LazyQueryResult/LazyQueryResu
 import Paper from '@mui/material/Paper'
 import { Elevations } from '../../theme/theme'
 import { useLazyResultsFromSelectionInterface } from '../../selection-interface/hooks'
-import { useStatusOfLazyResults } from '../../../js/model/LazyQueryResult/hooks'
+import {
+  useSelectedResults,
+  useStatusOfLazyResults,
+} from '../../../js/model/LazyQueryResult/hooks'
 import { useTheme } from '@mui/material/styles'
 import LinearProgress from '@mui/material/LinearProgress'
 import ViewAgendaIcon from '@mui/icons-material/ViewAgenda'
@@ -32,11 +35,107 @@ import { DarkDivider } from '../../dark-divider/dark-divider'
 import { ResultsCommonControls } from './table'
 import { TypedUserInstance } from '../../singletons/TypedUser'
 import user from '../../singletons/user-instance'
+import { VariableSizeList } from 'react-window'
+import { Memo } from '../../memo/memo'
 
 type Props = {
   mode: any
   setMode: any
   selectionInterface: any
+}
+
+function scrollToItem({
+  listRef,
+  index,
+  animationFrameId,
+  currentScrollOffset,
+  lastChangedTime,
+}: {
+  listRef: VariableSizeList
+  index: number
+  animationFrameId: React.MutableRefObject<number | null>
+  currentScrollOffset: React.MutableRefObject<number | null>
+  lastChangedTime: React.MutableRefObject<number | null>
+}) {
+  animationFrameId.current = window.requestAnimationFrame(() => {
+    const timeSinceLastChange = Date.now() - lastChangedTime.current!
+    if (timeSinceLastChange > 1000) {
+      return
+    }
+    if ((listRef.state as any).scrollOffset !== currentScrollOffset.current) {
+      lastChangedTime.current = Date.now()
+      currentScrollOffset.current = (listRef.state as any).scrollOffset
+    }
+    if (!(listRef.state as any).isScrolling) {
+      listRef.scrollToItem(index, 'smart')
+    }
+    scrollToItem({
+      listRef,
+      index,
+      animationFrameId,
+      currentScrollOffset,
+      lastChangedTime,
+    })
+  })
+}
+
+function startScrollingToItem({
+  listRef,
+  index,
+  animationFrameId,
+  currentScrollOffset,
+  lastChangedTime,
+}: {
+  listRef: VariableSizeList
+  index: number
+  animationFrameId: React.MutableRefObject<number | null>
+  currentScrollOffset: React.MutableRefObject<number | null>
+  lastChangedTime: React.MutableRefObject<number | null>
+}) {
+  lastChangedTime.current = Date.now()
+  currentScrollOffset.current = (listRef.state as any).scrollOffset
+  listRef.scrollToItem(index, 'smart')
+  scrollToItem({
+    listRef,
+    index,
+    animationFrameId,
+    currentScrollOffset,
+    lastChangedTime,
+  })
+}
+
+export const useScrollToItemOnSelection = ({
+  selectionInterface,
+}: {
+  selectionInterface: Props['selectionInterface']
+}) => {
+  const listRef = React.useRef<VariableSizeList | null>(null)
+
+  const lazyResults = useLazyResultsFromSelectionInterface({
+    selectionInterface,
+  })
+  const selectedResults = useSelectedResults({ lazyResults })
+  const animationFrameId = React.useRef<number | null>(null)
+  const currentScrollOffset = React.useRef<number | null>(null)
+  const lastChangedTime = React.useRef<number | null>(null)
+
+  React.useEffect(() => {
+    const allResults = Object.values(lazyResults.results)
+    const selected = Object.values(selectedResults)
+    if (listRef.current && selected.length >= 1) {
+      startScrollingToItem({
+        listRef: listRef.current,
+        index: allResults.indexOf(selected[0]),
+        animationFrameId,
+        currentScrollOffset,
+        lastChangedTime,
+      })
+    }
+    return () => {
+      window.cancelAnimationFrame(animationFrameId.current as number)
+    }
+  }, [selectedResults, lazyResults])
+  return { listRef }
 }
 
 const ResultCards = ({ mode, setMode, selectionInterface }: Props) => {
@@ -53,6 +152,8 @@ const ResultCards = ({ mode, setMode, selectionInterface }: Props) => {
    * This is solely to keep the illusion of responsiveness when switching from table mode to list mode (or dropping a new result visual in)
    */
   const [isMounted, setIsMounted] = React.useState(false)
+
+  const { listRef } = useScrollToItemOnSelection({ selectionInterface })
 
   React.useEffect(() => {
     const mountedTimeout = setTimeout(() => {
@@ -137,49 +238,56 @@ const ResultCards = ({ mode, setMode, selectionInterface }: Props) => {
       <Grid item className="w-full h-full p-2">
         <Paper elevation={Elevations.paper} className="w-full h-full">
           {isMounted ? (
-            <AutoVariableSizeList<LazyQueryResult, HTMLDivElement>
-              controlledMeasuring={true}
-              items={results}
-              defaultSize={60}
-              overscanCount={10}
-              Item={({ itemRef, item, measure, index, width }) => {
-                return (
-                  <div ref={itemRef} className="relative">
-                    {index !== 0 ? (
-                      <>
-                        <div className="h-min w-full Mui-bg-divider" />
-                      </>
-                    ) : null}
-                    <ResultItem
-                      lazyResults={results}
-                      lazyResult={item}
-                      selectionInterface={selectionInterface}
-                      measure={measure}
-                      index={index}
-                      width={width}
-                    />
-                    {index === results.length - 1 ? (
-                      <>
-                        <div className="h-min w-full Mui-bg-divider" />
-                      </>
-                    ) : null}
-                  </div>
-                )
-              }}
-              Empty={() => {
-                if (Object.values(status).length === 0) {
-                  return <div className="p-2">Search has not yet been run.</div>
-                }
-                if (isSearching) {
-                  return <LinearProgress variant="indeterminate" />
-                }
-                return (
-                  <div className="result-item-collection-empty p-2">
-                    No Results Found
-                  </div>
-                )
-              }}
-            />
+            <Memo
+              dependencies={[listRef.current, results, isSearching, status]}
+            >
+              <AutoVariableSizeList<LazyQueryResult, HTMLDivElement>
+                controlledMeasuring={true}
+                items={results}
+                defaultSize={60}
+                overscanCount={10}
+                Item={({ itemRef, item, measure, index, width }) => {
+                  return (
+                    <div ref={itemRef} className="relative">
+                      {index !== 0 ? (
+                        <>
+                          <div className="h-min w-full Mui-bg-divider" />
+                        </>
+                      ) : null}
+                      <ResultItem
+                        lazyResults={results}
+                        lazyResult={item}
+                        selectionInterface={selectionInterface}
+                        measure={measure}
+                        index={index}
+                        width={width}
+                      />
+                      {index === results.length - 1 ? (
+                        <>
+                          <div className="h-min w-full Mui-bg-divider" />
+                        </>
+                      ) : null}
+                    </div>
+                  )
+                }}
+                Empty={() => {
+                  if (Object.values(status).length === 0) {
+                    return (
+                      <div className="p-2">Search has not yet been run.</div>
+                    )
+                  }
+                  if (isSearching) {
+                    return <LinearProgress variant="indeterminate" />
+                  }
+                  return (
+                    <div className="result-item-collection-empty p-2">
+                      No Results Found
+                    </div>
+                  )
+                }}
+                variableSizeListRef={listRef}
+              />
+            </Memo>
           ) : (
             <LinearProgress variant="indeterminate" />
           )}
