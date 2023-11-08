07cefb8107313c4be41b23119635ccdba26f7a74
Joseph North <joernorth@gmail.com>
2023-11-08 14:56:46 -0700

    fix resultFilter initial draw issue

--- ui-frontend/packages/catalog-ui-search/src/main/webapp/component/visualization/maps/drawing-and-display.tsx
+++ ui-frontend/packages/catalog-ui-search/src/main/webapp/component/visualization/maps/drawing-and-display.tsx
@@ -272,7 +272,7 @@ export const useDrawingAndDisplayModels = ({
   }
   React.useEffect(() => {
     updateFilterModels()
-  }, [filterTree])
+  }, [filterTree, models])
   useListenTo(selectionInterface, 'change:currentQuery', updateFilterModels)
   useListenTo(
     TypedUserInstance.getPreferences(),
