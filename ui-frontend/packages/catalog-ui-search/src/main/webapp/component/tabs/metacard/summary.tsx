c94585ba37257b2d10a5b02d64dd43389d0b156b
zkirksey <148796355+zkirksey@users.noreply.github.com>
2024-01-18 16:58:13 -0500

    Multi-Valued date attributes can now be properly edited (#869)

--- ui-frontend/packages/catalog-ui-search/src/main/webapp/component/tabs/metacard/summary.tsx
+++ ui-frontend/packages/catalog-ui-search/src/main/webapp/component/tabs/metacard/summary.tsx
@@ -40,6 +40,7 @@ import { useConfiguration } from '../../../js/model/Startup/configuration.hooks'
 import { useMetacardDefinitions } from '../../../js/model/Startup/metacard-definitions.hooks'
 import Common from '../../../js/Common'
 import SummaryManageAttributes from '../../../react-component/summary-manage-attributes/summary-manage-attributes'
+import moment from 'moment-timezone'
 
 type Props = {
   result: LazyQueryResult
@@ -364,10 +365,16 @@ export const Editor = ({
             setMode(Mode.Saving)
             let transformedValues = values
             try {
-              transformedValues =
-                attrType === 'BINARY'
-                  ? values.map((subval: any) => subval.split(',')[1])
-                  : values
+                switch (attrType) {
+                    case 'BINARY':
+                        transformedValues = values.map((subval: any) => subval.split(',')[1])
+                        break;
+                    case 'DATE':
+                        transformedValues = values.map((subval: any) => moment(subval).toISOString())
+                        break;
+                    default:
+                        transformedValues = values
+                }
             } catch (err) {
               console.error(err)
             }
