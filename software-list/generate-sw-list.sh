c55297a8327555327a5014cb2ad4058f157eaacc
Alexa Bird <alexa.bird@octo.us>
2025-03-19 07:10:11 -0700

    Remove 'project' line for dependency output

--- software-list/generate-sw-list.sh
+++ software-list/generate-sw-list.sh
@@ -1,3 +1,2 @@
 # generate the software list
 mvn dependency:list -DexcludeTransitive=true -DincludeScope=runtime -DexcludeGroupIds=com.connexta,ddf,org.codice  -pl '!docs,!ui-frontend' | grep -i compile | sort | uniq -u | cut -d ":" -f 2,4 | sed s/:/-/g > ddf-ui-deplist.out
-echo "Project" >> ddf-ui-deplist.out
