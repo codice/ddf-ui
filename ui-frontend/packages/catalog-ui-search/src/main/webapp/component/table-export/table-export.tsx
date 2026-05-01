b278bd0e5d85f2c33779cffa33b2ea6211a97983
Matthew Smith <matthew.smith@w-work-37.rdmz.isridev.com>
2026-05-01 16:00:34 +1000

    Fixed typo in export note

--- ui-frontend/packages/catalog-ui-search/src/main/webapp/component/table-export/table-export.tsx
+++ ui-frontend/packages/catalog-ui-search/src/main/webapp/component/table-export/table-export.tsx
@@ -388,7 +388,7 @@ const TableExports = ({
             {['csv'].includes(exportFormat) ? (
                 <div className="csv-format-warning text-center pt-1" style={{ marginTop: '10px'}}>
                   <span>
-                    NOTE: Complex data exported as CSV may not be formattedorrectly if opened directly.
+                    NOTE: Complex data exported as CSV may not be formatted correctly if opened directly.
                     For best results, open Excel first and then select the file using File → Open.
                   </span>
                 </div>
